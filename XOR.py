8d4fd4e639dd8dde37d8e05bb507a40cfa90e3e9
ybiryukov <marinero989@gmail.com>
2022-10-17 22:28:56 +0300

    added XOR.py

---
+++ XOR.py
@@ -0,0 +1,37 @@
+train = [
+    {
+        "args": [0, 0],
+        "correct_answer": 0
+    },
+    {
+        "args": [1, 0],
+        "correct_answer": 0
+    },
+    {
+        "args": [0, 1],
+        "correct_answer": 1
+    },
+    {
+        "args": [1, 1],
+        "correct_answer": 0
+    }
+]
+
+test = [
+    {
+        "args": [0, 0],
+        "correct_answer": 0
+    },
+    {
+        "args": [1, 0],
+        "correct_answer": 0
+    },
+    {
+        "args": [0, 1],
+        "correct_answer": 1
+    },
+    {
+        "args": [1, 1],
+        "correct_answer": 0
+    }
+]
